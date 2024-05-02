ca36f6c4e310120ea278d00a4772e30c58181438
juliadietlmeier <79544193+juliadietlmeier@users.noreply.github.com>
2024-05-02 18:16:20 +0100

    Update

--- apply_crf.py
+++ apply_crf.py
@@ -1,30 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy 
 import pydensecrf.densecrf as dcrf
-
-d = dcrf.DenseCRF2D(640, 480, 5)  # width, height, nlabels
 from pydensecrf.utils import unary_from_softmax, create_pairwise_bilateral
 from pydensecrf.utils import unary_from_labels, create_pairwise_bilateral, create_pairwise_gaussian
-import cv2
-from skimage import color, data, filters, graph, measure, morphology
 import numpy as np
-from skimage.morphology import erosion, dilation, opening, closing, white_tophat  # noqa
-from skimage.morphology import black_tophat, skeletonize, convex_hull_image  # noqa
-from skimage.morphology import disk  # noqa
 
-def plot_comparison(original, filtered, filter_name):
-    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4), sharex=True, sharey=True)
-    ax1.imshow(original, cmap=plt.cm.gray)
-    ax1.set_title('original')
-    ax1.set_axis_off()
-    ax2.imshow(filtered, cmap=plt.cm.gray)
-    ax2.set_title(filter_name)
-    ax2.set_axis_off()
-
-def apply_crf(im, pred):
-  im = numpy.ascontiguousarray(im)
+def computeCRF(im, pred):
+  im = np.ascontiguousarray(im)
 
   d = dcrf.DenseCRF2D(im.shape[1], im.shape[0], 2)  # width, height, nlabels
   
@@ -34,6 +17,6 @@ def apply_crf(im, pred):
   d.addPairwiseGaussian(sxy=0.220880737269, compat=1.24845093352)
   d.addPairwiseBilateral(sxy=22.3761305044, srgb=7.70254062277, rgbim=im, compat=1.40326787165)
   processed = d.inference(12)
-  res = numpy.argmax(processed, axis=0).reshape(im.shape[:2])
+  res = np.argmax(processed, axis=0).reshape(im.shape[:2])
 
   return res 
